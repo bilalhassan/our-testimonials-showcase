9442798d7fc88320cba7216bf89c8f41aa12f3be
Bilal Hassan <info@ottawapc.ca>
2015-02-03 09:42:37 -0500

    Create init.php
    a file with comments on the initial requirements for the plugin

---
+++ init.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+  Plugin Name: Our Testimonials Showcase
+  Description: Showcase your customer feedback on your site is a creative and appealing way.
+ */
+ 
+ // create a function that is called when the plugin is activated
+ // this function will declare constants for the plugin URL, the plugin directory path and the plugin version
+ // it will also create the plugin options
+ 
+ 
+ // create a function that is called when the plugin is de-activated
+ // this function is blank for now
+
+ 
+ // create a menu in wordpress for the new plugin
+ // link the menu to a php file which will have the options form
+
+ // create a function that will be used to load styles and scripts for the site frontend
+
+ // create a custom post type "testimonial"
+
+ // create the options form, add a save button
+ // 
